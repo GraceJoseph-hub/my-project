7c3a3a95ca3c15513a195e5a60d986463856fc71
Grace Joseph <gracecjoseph5@gmail.com>
2025-04-22 11:03:35 +0100

    Added a text color to the navlinks

--- src/component/navlinks/Navlinks.jsx
+++ src/component/navlinks/Navlinks.jsx
@@ -1,76 +1,3 @@
-// import React, { useState } from "react";
-// import "./Navlinks.css";
-// import { NavLink, useLocation } from "react-router-dom";
-// import { FaBars, FaSquareXmark } from "react-icons/fa6";
-
-// const Navlinks = () => {
-//   const location = useLocation();
-//   const activeRoute = location.pathname;
-//   const [isOpen, setIsOpen] = useState(false);
-
-//   const toggleBtn = () => {
-//     setIsOpen(!isOpen);
-//   };
-
-//   const links = [
-//     {
-//       path: "/",
-//       name: "Home",
-//     },
-//     {
-//       path: "/search",
-//       name: "Search",
-//     },
-//     {
-//       path: "/about",
-//       name: "About",
-//     },
-//   ];
-
-//   return (
-//     <>
-//       <div className="navlinks">
-//         {links.map((link, index) => (
-//           <NavLink
-//             to={link.path}
-//             key={`${link.name}-${index}`}
-//             className={` lowercase mr-8 text-sm ${
-//               activeRoute === link.path
-//                 ? "text-yellow"
-//                 : "text-black dark:text-white"
-//             }`}
-//           >
-//             {link.name}
-//           </NavLink>
-//         ))}
-//       </div>
-//       <div onClick={toggleBtn} className="absolute right-2 cursor-pointer">
-//         {isOpen ? <FaSquareXmark /> : <FaBars />}
-//       </div>
-//       <div className={`overlay ${isOpen ? "open" : "hide"}`}>
-//         <div className="flex flex-col items-center">
-//           {links.map((link, index) => (
-//             <NavLink
-//               to={link.path}
-//               key={`${link.name}-${index}`}
-//               className={`text-base mt-2 mb-2 ${
-//                 activeRoute === link.path
-//                   ? "text-yellow"
-//                   : "text-white"
-//               }`}
-//             >
-//               {link.name}
-//             </NavLink>
-//           ))}
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Navlinks;
-
-
 import React, { useState } from "react";
 import "./Navlinks.css";
 import { NavLink, useLocation } from "react-router-dom";
@@ -110,7 +37,7 @@ const Navlinks = () => {
             className={`lowercase mr-8 text-sm ${
               activeRoute === link.path
                 ? "text-yellow"
-                : "text-black dark:text-white"
+                : "text-black dark:text-black"
             }`}
           >
             {link.name}
@@ -129,7 +56,7 @@ const Navlinks = () => {
               className={`text-base mt-2 mb-2 ${
                 activeRoute === link.path ? "text-yellow" : "text-white"
               }`}
-              onClick={toggleBtn} // Close the overlay when a link is clicked
+              onClick={toggleBtn} 
             >
               {link.name}
             </NavLink>
